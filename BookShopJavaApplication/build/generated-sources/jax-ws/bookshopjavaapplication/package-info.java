2dab88f5855ee33ff04279f2b42aa0d32e041d5d
Daniel Tšarin <datsar@ttu.ee>
2018-05-22 18:15:28 +0300

    main Fix

--- BookShopJavaApplication/build/generated-sources/jax-ws/bookshopjavaapplication/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ttu.ee/idu0075/2015/ws/bookShop", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package bookshopjavaapplication;
